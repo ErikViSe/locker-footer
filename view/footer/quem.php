afc73390b2423882aff1780db37b1a6160585dc3
ErikViSe <eriksechi@gmail.com>
2022-11-07 18:52:06 -0300

    COMEÇANDO VIEW FOOTER "QUEM SOMOS"

---
+++ view/footer/quem.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quem somos</title>
+</head>
+<body>
+    <?php include "public/header.php" ?>
+
+    <h1> Nossa história </h1>
+    
+    <p>O projeto Locketec surgiu como uma proposta de Trabalho de conclusão de curso (TCC) proposto pela Etec Irmã Agostina.
+        Fomos apresentados à essa ideia como uma maneira de assistir com as necessidades da mesma.
+    </p>
+</body>
+</html>
