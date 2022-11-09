8cb773643d767d4876948e716369033abf2ceef8
ErikViSe <eriksechi@gmail.com>
2022-11-08 22:10:49 -0300

    Adicionando CSS do inicio da tela

--- view/footer/quem.php
+++ view/footer/quem.php
@@ -4,15 +4,188 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../public/assets/css/quem.css">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+
     <title>Quem somos</title>
 </head>
 <body>
-    <?php include "public/header.php" ?>
+    
+    <?php include "../../public/header.php"?>
 
-    <h1> Nossa história </h1>
+    <H1>QUEM SOMOS</H1>     <br> <br>
+
+<div class="container">
+  <div class="card-group vgr-cards">
+    <div class="card">
+      <div class="card-img-body">
+      
+      </div>
+      <div class="card-body">
+        <h4 class="card-title">Nossa história <i class="bi bi-book"></i></h4>
+        <br>
+        
+        <p class="card-text">Projeto iniciado/criado em 2022</p>
+        <p class="card-text">O projeto Locketec surgiu como uma proposta de Trabalho de conclusão de curso (TCC) proposto pela Etec Irmã Agostina.
+        Fomos apresentados à essa ideia como uma maneira de assistir com as necessidades da mesma.</p>
+     
+      </div>
+    </div> 
+  </div>
+</div>
+
+    <br><br>
+
+<div class="container">
+  <div class="card-group vgr-cards">
+    <div class="card">
+      <div class="card-img-body">
+      
+      </div>
+      <div class="card-body">
+        <h4 class="card-title">Nosso Objetivo  </h4>
+        <br>
+        <p class="card-text">Visamos criar um site onde o Aluno/cliente tenha uma experiência dinâmica e facil na hora de alugar seu armário.</p>
+     
+      </div>
+    </div> 
+  </div>
+</div>
+
+
+
+ 
+
+    <!-- <h2> Nossa história </h2>
     
     <p>O projeto Locketec surgiu como uma proposta de Trabalho de conclusão de curso (TCC) proposto pela Etec Irmã Agostina.
         Fomos apresentados à essa ideia como uma maneira de assistir com as necessidades da mesma.
     </p>
+
+    <div class="text">
+    <p>Visamos criar um site onde o Aluno/cliente tenha uma experiência dinâmica e facil na hora de alugar seu armário.</p>
+    </div>
+
+    <h2>Nossa Equipe</h2> -->
+
+<section>
+    <div class="container">
+    	<div class="row">
+
+    		
+    		<!-- Erik, Erick e Rossi-->
+    		<div class="col-md-4">
+    		    <div class="card profile-card-3">
+    		        <div class="background-block">
+    		            <img src="../../public/assets/img/espaço.jpg" alt="profile-sample1" class="background"/>
+    		        </div>
+    		        <div class="profile-thumb-block">
+    		            <img src="../../public/assets/img/maco.jpg" alt="profile-image" class="profile"/>
+    		        </div>
+    		        <div class="card-content">
+                    <h2>Erik Sechi<small>Desenvolvedor Estudante</small></h3>
+                    <div class="icon-block"><a href="#"><i class="fa fa-github"></i></a><a href="#"> <i class="fa fa-linkedin"></i></a><a href="#"> <i class="fa fa-instagram"></i></a></div>
+                    </div>
+                </div>
+    		</div>
+
+            <div class="col-md-4">
+    		    <div class="card profile-card-3">
+    		        <div class="background-block">
+    		            <img src="../../public/assets/img/selva.jpg" alt="profile-sample1" class="background"/>
+    		        </div>
+    		        <div class="profile-thumb-block">
+    		            <img src="../../public/assets/img/golira.jpg" alt="profile-image" class="profile"/>
+    		        </div>
+    		        <div class="card-content">
+                    <h2>Erick Guimarães<small>Desenvolvedor Estudante</small></h3>
+                    <div class="icon-block"><a href="#"><i class="fa fa-github"></i></a><a href="#"> <i class="fa fa-linkedin"></i></a><a href="#"> <i class="fa fa-instagram"></i></a></div>
+                    </div>
+                </div>
+    		</div>
+
+            <div class="col-md-4">
+    		    <div class="card profile-card-3">
+    		        <div class="background-block">
+    		            <img src="../../public/assets/img/zoo.jpg" alt="profile-sample1" class="background"/>
+    		        </div>
+    		        <div class="profile-thumb-block">
+    		            <img src="../../public/assets/img/kayck.jpg" alt="profile-image" class="profile"/>
+    		        </div>
+    		        <div class="card-content">
+                    <h2>Kayck Rossi<small>Desenvolvedor Estudante</small></h3>
+                    <div class="icon-block"><a href="#"><i class="fa fa-github"></i></a><a href="#"> <i class="fa fa-linkedin"></i></a><a href="#"> <i class="fa fa-instagram"></i></a></div>
+                    </div>
+                </div>
+    		</div>
+    		
+    	
+    		
+    	</div>
+        <br>
+        <!-- Segunda Linha -->
+
+        <div class="row">
+
+    		
+    		<!-- Ana, Kishida e Kayk-->
+    		<div class="col-md-4">
+    		    <div class="card profile-card-3">
+    		        <div class="background-block">
+    		            <img src="../../public/assets/img/espaço.jpg" alt="profile-sample1" class="background"/>
+    		        </div>
+    		        <div class="profile-thumb-block">
+    		            <img src="../../public/assets/img/" alt="profile-image" class="profile"/>
+    		        </div>
+    		        <div class="card-content">
+                    <h2> Ana Luiza<small>Desenvolvedor Estudante</small></h3>
+                    <div class="icon-block"><a href="#"><i class="fa fa-github"></i></a><a href="#"> <i class="fa fa-linkedin"></i></a><a href="#"> <i class="fa fa-instagram"></i></a></div>
+                    </div>
+                </div>
+    		</div>
+
+            <div class="col-md-4">
+    		    <div class="card profile-card-3">
+    		        <div class="background-block">
+    		            <img src="../../public/assets/img/selva.jpg" alt="profile-sample1" class="background"/>
+    		        </div>
+    		        <div class="profile-thumb-block">
+    		            <img src="../../public/assets/img/#" alt="profile-image" class="profile"/>
+    		        </div>
+    		        <div class="card-content">
+                    <h2>Guilherme Kishida<small>Desenvolvedor Estudante</small></h3>
+                    <div class="icon-block"><a href="#"><i class="fa fa-github"></i></a><a href="#"> <i class="fa fa-linkedin"></i></a><a href="#"> <i class="fa fa-instagram"></i></a></div>
+                    </div>
+                </div>
+    		</div>
+
+            <div class="col-md-4">
+    		    <div class="card profile-card-3">
+    		        <div class="background-block">
+    		            <img src="../../public/assets/img/zoo.jpg" alt="profile-sample1" class="background"/>
+    		        </div>
+    		        <div class="profile-thumb-block">
+    		            <img src="../../public/assets/img/#" alt="profile-image" class="profile"/>
+    		        </div>
+    		        <div class="card-content">
+                    <h2>Kayk Oliveira<small>Desenvolvedor Estudante</small></h3>
+                    <div class="icon-block"><a href="#"><i class="fa fa-github"></i></a><a href="#"> <i class="fa fa-linkedin"></i></a><a href="#"> <i class="fa fa-instagram"></i></a></div>
+                    </div>
+                </div>
+    		</div>
+    		
+    	
+    		
+    	</div>
+
+    </div>
+</section>
+
+
+
+    <?php include "../../public/footer.html" ?>
 </body>
 </html>
